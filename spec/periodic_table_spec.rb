c184564a308a011b6eb9486d518bd95062a8bd35
Yatin Sarbalia <yatinsns@gmail.com>
2016-01-04 01:12:17 +0530

    Fix test

--- spec/periodic_table_spec.rb
+++ spec/periodic_table_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe PeriodicTable do
-  it 'has a version number' do
-    expect(PeriodicTable::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'should return data for a named element' do
+    element_data = PeriodicTable.lookup('oxygen')
+    element_data.should_not be_nil
+    element_data.symbol.should == 'O'
+    element_data.atomic_weight.should == '15.9994'
   end
 end
