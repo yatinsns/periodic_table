d34e49355e78059cb959c0d602884d1a185e9e47
Yatin Sarbalia <yatinsns@gmail.com>
2016-01-04 01:02:20 +0530

    Initial commit

---
+++ spec/periodic_table_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe PeriodicTable do
+  it 'has a version number' do
+    expect(PeriodicTable::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
